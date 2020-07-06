d892505abbf98c202d6a4b3cd13a56f6ed22e325
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-06 11:48:46 +0200

    Create new file for boolean transformation functions.

---
+++ src/Refinery/KindlyTo/Transformation/BooleanTransformation.php
@@ -0,0 +1,21 @@
+<?php
+/* Copyright (c) 1998-2019 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author Luka Stocker <lstocker@concepts-and-training.de>
+ */
+
+namespace ILIAS\Refinery\KindlyTo\Transformation;
+
+use ILIAS\Data\Result;
+use ILIAS\Refinery\DeriveApplyToFromTransform;
+use ILIAS\Refinery\Transformation;
+use ILIAS\Refinery\ConstraintViolationException;
+
+class StringTransformation implements Transformation
+{
+    use DeriveApplyToFromTransform;
+
+
+
+}
