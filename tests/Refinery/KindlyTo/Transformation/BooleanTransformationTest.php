b7ba97e255b979de2ade909d01b4ddac06971eeb
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-06 11:51:36 +0200

    Create file for testing boolean transformation.

---
+++ tests/Refinery/KindlyTo/Transformation/BooleanTransformationTest.php
@@ -0,0 +1,24 @@
+<?php
+/* Copyright (c) 1998-2020 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author Luka Stocker <lstocker@concepts-and-training.de>
+ */
+
+namespace ILIAS\Tests\Refinery\KindlyTo\Transformation;
+
+require_once('./libs/composer/vendor/autoload.php');
+
+use ILIAS\Data\Result;
+use ILIAS\Refinery\KindlyTo\Transformation\StringTransformation;
+use ILIAS\Tests\Refinery\TestCase;
+
+/**
+* Test transformations in this Group
+*/
+class KindlyToBooleanTransformationTest extends TestCase
+{
+
+
+
+}
