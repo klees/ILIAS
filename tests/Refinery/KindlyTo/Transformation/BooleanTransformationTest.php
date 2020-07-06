4788c8387d0502ce94fd23689a5138e6a2b66201
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-06 15:26:39 +0200

    Delete reference to unused path.

--- tests/Refinery/KindlyTo/Transformation/BooleanTransformationTest.php
+++ tests/Refinery/KindlyTo/Transformation/BooleanTransformationTest.php
@@ -9,7 +9,6 @@ namespace ILIAS\Tests\Refinery\KindlyTo\Transformation;
 
 require_once('./libs/composer/vendor/autoload.php');
 
-use ILIAS\Data\Result;
 use ILIAS\Refinery\KindlyTo\Transformation\BooleanTransformation;
 use ILIAS\Tests\Refinery\TestCase;
 
