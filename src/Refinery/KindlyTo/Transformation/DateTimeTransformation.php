9ecd68700ff7feb79548b7a9d622405b2ab7b34d
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-07 15:10:11 +0200

    Transform to DateTimeImmutable.

--- src/Refinery/KindlyTo/Transformation/DateTimeTransformation.php
+++ src/Refinery/KindlyTo/Transformation/DateTimeTransformation.php
@@ -36,7 +36,7 @@ class DateTimeTransformation implements Transformation
      */
     public function transform($from)
     {
-        $from = new DateTime('$from');
+        $from = new DateTime($from);
         if($ts = $from->format(DtAtom || DtCookie || DtISO8601 || DtRFC822 || DtRFC850 || DtRFC1036 || DtRFC1123 ||
         DtRFC7231 || DtRFC2822 || DtRFC3339 || DtRFC3339ext || DtRSS || DtW3C))
         {
