8c27cbc427f0d6be9907d590a6192ff5efdfb856
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-07 13:29:52 +0200

    Transform DateTimeInterface formats into DateTimeImmutable and Integer into Unix timestamps.

---
+++ src/Refinery/KindlyTo/Transformation/DateTimeTransformation.php
@@ -0,0 +1,72 @@
+<?php
+/* Copyright (c) 1998-2020 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author Luka Stocker <lstocker@concepts-and-training.de>
+ */
+
+namespace ILIAS\Refinery\KindlyTo\Transformation;
+
+use DateTime;
+use ILIAS\Data\Result;
+use ILIAS\Refinery\DeriveApplyToFromTransform;
+use ILIAS\Refinery\Transformation;
+use ILIAS\Refinery\ConstraintViolationException;
+
+const DtAtom = 'Y-m-d\TH:i:sP';
+const DtCookie = 'l, d-M-Y H:i:s T';
+const DtISO8601 = 'Y-m-d\TH:i:sO';
+const DtRFC822 = 'D, d M y H:i:s O';
+const DtRFC850 = 'l, d-M-y H:i:s T';
+const DtRFC1036 = 'D, d M y H:i:s O';
+const DtRFC1123 = 'D, d M y H:i:s O';
+const DtRFC7231 = 'D, d M Y H:i:s \G\M\T';
+const DtRFC2822 = 'D, d M Y H:i:s O';
+const DtRFC3339 = 'Y-m-d\TH:i:sP';
+const DtRFC3339ext = 'Y-m-d\TH:i:s.vP';
+const DtRSS = 'D, d M Y H:i:s O';
+const DtW3C = 'Y-m-d\TH:i:sP';
+
+class DateTimeTransformation implements Transformation
+{
+    use DeriveApplyToFromTransform;
+
+    /**
+     * @inheritdoc
+     */
+    public function transform($from)
+    {
+        if($ts = $from->format(DtAtom || DtCookie || DtISO8601 || DtRFC822 || DtRFC850 || DtRFC1036 || DtRFC1123 ||
+        DtRFC7231 || DtRFC2822 || DtRFC3339 || DtRFC3339ext || DtRSS || DtW3C))
+        {
+            return $DateImmutable = new \DateTimeImmutable("$from");
+        }
+        elseif(true === is_int($from))
+        {
+            $date = new DateTime('$from');
+            return date_timestamp_get($date);
+        }
+        else
+        {
+            throw new ConstraintViolationException(
+                'The value could not be transformed into a DateTimeImmutable or Unix Timestamp.',
+                ''
+            );
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function applyTo(Result $data): Result
+    {
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __invoke($from)
+    {
+        return $this->transform($from);
+    }
+}
