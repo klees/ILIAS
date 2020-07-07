028d4769868127daee72356739851aafdeb41d15
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-07 08:08:35 +0200

    Implement a new file for a transformation of all data to a dictionary.

--- src/Refinery/KindlyTo/Group.php
+++ src/Refinery/KindlyTo/Group.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 namespace ILIAS\Refinery\KindlyTo;
 
 use ILIAS\Refinery\KindlyTo\Transformation\BooleanTransformation;
+use ILIAS\Refinery\KindlyTo\Transformation\DictionaryTransformation;
 use ILIAS\Refinery\KindlyTo\Transformation\StringTransformation;
 use ILIAS\Refinery\Transformation;
 use SimpleSAML\TwigConfigurableI18n\Twig\Extensions\Node\Trans;
