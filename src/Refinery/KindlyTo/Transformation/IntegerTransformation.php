a43ecf62dfb4be77467dfe67a4710126ad6471b7
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-08 14:57:56 +0200

    Transform float and string into integer.

--- src/Refinery/KindlyTo/Transformation/IntegerTransformation.php
+++ src/Refinery/KindlyTo/Transformation/IntegerTransformation.php
@@ -12,13 +12,28 @@ use ILIAS\Refinery\DeriveApplyToFromTransform;
 use ILIAS\Refinery\Transformation;
 use ILIAS\Refinery\ConstraintViolationException;
 
+const RegInt = '\s*(0|(-?[1-9]\d*))\s*';
+
 class IntegerTransformation implements Transformation
 {
     use DeriveApplyToFromTransform;
 
     public function transform($from)
     {
-
+        if(true === is_float($from))
+        {
+            $from = round($from);
+            $from = intval($from);
+            return $from;
+        }
+        if(true === is_string($from))
+        {
+            if(preg_match(RegInt, $from, $RegMatch))
+            {
+                $from = floatval($from);
+                return $from;
+            }
+        }
     }
 
 
