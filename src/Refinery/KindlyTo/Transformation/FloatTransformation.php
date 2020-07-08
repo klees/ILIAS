3ef13665868ea6a5b15e51e84814fde33a342a64
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-08 17:01:49 +0200

    Implement base code for integration later on.

---
+++ src/Refinery/KindlyTo/Transformation/FloatTransformation.php
@@ -0,0 +1,35 @@
+<?php
+/* Copyright (c) 1998-2020 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author Luka Stocker <lstocker@concepts-and-training.de>
+ */
+
+namespace ILIAS\Refinery\KindlyTo\Transformation;
+
+use ILIAS\Data\Result;
+use ILIAS\Refinery\DeriveApplyToFromTransform;
+use ILIAS\Refinery\Transformation;
+use ILIAS\Refinery\ConstraintViolationException;
+
+class FloatTransformation implements Transformation
+{
+    use DeriveApplyToFromTransform;
+
+    public function transform($from)
+    {
+
+    }
+
+
+
+    public function applyTo(Result $data): Result
+    {
+    }
+
+    public function __invoke($from)
+    {
+        return $this->transform($from);
+    }
+}
+
