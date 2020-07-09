5ab5aa842cb68e07a8985bcc60e6b42f09d769f0
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-09 17:10:18 +0200

    Check Format of value with RegEx.

--- src/Refinery/KindlyTo/Transformation/DateTimeTransformation.php
+++ src/Refinery/KindlyTo/Transformation/DateTimeTransformation.php
@@ -36,7 +36,7 @@ class DateTimeTransformation implements Transformation
     {
         if(TRUE === is_string($from))
         {
-            if(\DateTimeImmutable::createFromFormat(\DateTimeImmutable::ATOM, $from) !== False)
+            if(preg_match(self::Reg_Atom, $from, $RegMatch))
             {
                 $DateImmutable = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::ATOM, $from);
                 return $DateImmutable->format(\DateTimeImmutable::ATOM);
