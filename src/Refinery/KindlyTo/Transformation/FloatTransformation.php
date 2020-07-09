d1b2e14d53fd18a2b76b9db9590877b387e3dc79
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-09 10:31:17 +0200

    Transform int and bool to float.

--- src/Refinery/KindlyTo/Transformation/FloatTransformation.php
+++ src/Refinery/KindlyTo/Transformation/FloatTransformation.php
@@ -12,16 +12,54 @@ use ILIAS\Refinery\DeriveApplyToFromTransform;
 use ILIAS\Refinery\Transformation;
 use ILIAS\Refinery\ConstraintViolationException;
 
+const RegString = '/\s*(0|(-?[1-9]\d*([.,]\d+)?))\s*/';
+const RegStringFloating = '/\s*-?\d+[eE]-?\d+\s*/';
+
 class FloatTransformation implements Transformation
 {
     use DeriveApplyToFromTransform;
 
     public function transform($from)
     {
+        if(true === is_int($from))
+        {
+            $from = (float)$from;
+            return $from;
+        }
+        elseif(true === is_bool($from))
+        {
+            $from = floatval(str_replace(',','.', str_replace('.','', $from)));
+            return $from;
+        }
+        elseif(true === is_string($from))
+        {
+            if(preg_match(RegString, $from, $RegMatch))
+            {
+
+            }
+            elseif(preg_match(RegStringFloating, $from, $RegMatch))
+            {
+
+            }
+            else
+            {
+                throw new ConstraintViolationException(
+                    'The string could not be transformed into an float',
+                    'not_float'
+                );
+            }
+        }
+        else
+        {
+            throw new ConstraintViolationException(
+                'The value could not be transformed into an float',
+                'not_float'
+            );
+        }
+
+
 
     }
-
-
 
     public function applyTo(Result $data): Result
     {
