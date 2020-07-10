d78d99ceda27abf22192cbcc24c79a864b934a8f
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-10 10:38:05 +0200

    Transform arrays to list and non-arrays to arrays.

--- src/Refinery/KindlyTo/Transformation/ListTransformation.php
+++ src/Refinery/KindlyTo/Transformation/ListTransformation.php
@@ -34,7 +34,33 @@ class ListTransformation implements Transformation
      */
     public function transform($from)
     {
+        if(false == is_array($from))
+        {
+            $from = array($from);
+            if(array_values() === $from)
+            {
+               throw new ConstraintViolationException(
+                   'The array ist empty',
+                   'value_array_is_empty'
+               ) ;
+            }
+        }
 
+        if(array_values() === $from)
+        {
+            throw new ConstraintViolationException(
+            'The array ist empty',
+            'value_array_is_empty'
+            ) ;
+        }
+
+        $result = array();
+        foreach($from as $val)
+        {
+            $transformedVal = $this->transformation->transform($val);
+            $result[] = $transformedVal;
+        }
+        return $result;
     }
 
     public function applyTo(Result $data): Result
