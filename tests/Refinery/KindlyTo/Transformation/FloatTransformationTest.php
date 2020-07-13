f1d86621291040d6bd2e3b8ae9f6774af25ae34a
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-13 14:35:27 +0200

    Check if InstanceOf is true or false.

--- tests/Refinery/KindlyTo/Transformation/FloatTransformationTest.php
+++ tests/Refinery/KindlyTo/Transformation/FloatTransformationTest.php
@@ -35,7 +35,7 @@ class FloatTransformationTest extends TestCase
     public function testFloatTransformation($originVal, $expectedVal)
     {
         $transformedValue = $this->transformation->transform($originVal);
-        $this->assertInstanceOf($expectedVal,$originVal, 'Failed InstanceOf');
+        $this->assertInstanceOf($expectedVal,FloatTransformation::class, 'Failed InstanceOf');
         $this->assertEquals($expectedVal, $transformedValue);
     }
 
