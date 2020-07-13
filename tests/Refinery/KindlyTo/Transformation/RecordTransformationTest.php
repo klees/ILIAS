d6c55ef9547217b5ffec382e9c8ee8370c55ab4e
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-13 09:36:19 +0200

    Test array to record transformation.

--- tests/Refinery/KindlyTo/Transformation/RecordTransformationTest.php
+++ tests/Refinery/KindlyTo/Transformation/RecordTransformationTest.php
@@ -7,17 +7,30 @@
 
 namespace ILIAS\Tests\Refinery\KindlyTo\Transformation;
 
-/**use ILIAS\Data\Result\Ok;*/
 use ILIAS\Refinery\KindlyTo\Transformation\IntegerTransformation;
 use ILIAS\Refinery\KindlyTo\Transformation\RecordTransformation;
 use ILIAS\Refinery\KindlyTo\Transformation\StringTransformation;
-/**use ILIAS\Refinery\ConstraintViolationException;*/
 use ILIAS\Tests\Refinery\TestCase;
 
 require_once('./libs/composer/vendor/autoload.php');
 
 class RecordTransformationTest extends TestCase
 {
+    const arr_String_Input = 'hello';
+    const arr_Int_Input = 1;
+    const string_key = 'stringKey';
+    const int_key = 'integerKey';
 
+    public function testRecordTransformation()
+    {
+        $recTransform = new RecordTransformation(
+            array(
+                self::string_key => new StringTransformation(),
+                self::int_key => new IntegerTransformation()
+            )
+        );
+        $transformedValue = $recTransform->transform(array(self::string_key => self::arr_String_Input, self::int_key => self::arr_Int_Input));
+        $this->assertEquals(array(self::string_key => self::arr_String_Input, self::int_key => self::arr_Int_Input), $transformedValue);
+    }
 
 }
