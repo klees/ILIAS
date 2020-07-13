d22de444ab0175c887bb0ce5364df674777eabe3
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-13 09:11:13 +0200

    Create base structure for the record transformt test file.

---
+++ tests/Refinery/KindlyTo/Transformation/RecordTransformationTest.php
@@ -0,0 +1,23 @@
+<?php
+/* Copyright (c) 1998-2020 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * @author  Luka Stocker <lstocker@concepts-and-training.de>
+ */
+
+namespace ILIAS\Tests\Refinery\KindlyTo\Transformation;
+
+/**use ILIAS\Data\Result\Ok;*/
+use ILIAS\Refinery\KindlyTo\Transformation\IntegerTransformation;
+use ILIAS\Refinery\KindlyTo\Transformation\RecordTransformation;
+use ILIAS\Refinery\KindlyTo\Transformation\StringTransformation;
+/**use ILIAS\Refinery\ConstraintViolationException;*/
+use ILIAS\Tests\Refinery\TestCase;
+
+require_once('./libs/composer/vendor/autoload.php');
+
+class RecordTransformationTest extends TestCase
+{
+
+
+}
