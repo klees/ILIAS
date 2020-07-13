3c57d7d66284dbd64d15c38bf57b9d52d78421a5
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-13 17:28:32 +0200

    Check if result is intsance of boolean.

--- tests/Refinery/KindlyTo/Transformation/BooleanTransformationTest.php
+++ tests/Refinery/KindlyTo/Transformation/BooleanTransformationTest.php
@@ -33,13 +33,13 @@ class BooleanTransformationTest extends TestCase
      /**
      * @dataProvider BooleanTestDataProvider
      * @param string $originVal
-     * @param boolean $expectedVal
+     * @param bool $expectedVal
      */
     public function testBooleanTransformation(string $originVal, bool $expectedVal)
     {
             $transformedValue = $this->transformation->transform($originVal);
             $this->assertIsBool($transformedValue, '');
-            $this->assertInstanceOf(BooleanTransformation::class, $originVal, '');
+            
             $this->assertSame($expectedVal, $transformedValue);
     }
 
