9053752f7a90e8a658adf5c8fb0474435719f18d
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-14 10:15:57 +0200

    Test transform with DataProvider.

--- tests/Refinery/KindlyTo/Transformation/RecordTransformationTest.php
+++ tests/Refinery/KindlyTo/Transformation/RecordTransformationTest.php
@@ -30,6 +30,7 @@ class RecordTransformationTest extends TestCase
             )
         );
         $transformedValue = $recTransform->transform(array(self::string_key => self::arr_String_Input, self::int_key => self::arr_Int_Input));
+        $this->assertIsArray($transformedValue, '');
         $this->assertEquals(array(self::string_key => self::arr_String_Input, self::int_key => self::arr_Int_Input), $transformedValue);
     }
 }
