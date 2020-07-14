2e0c1abc3a629bfa213f811067f18a37e7eee454
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-14 16:14:07 +0200

    Test failing transformations.

--- tests/Refinery/KindlyTo/Transformation/DictionaryTransformationTest.php
+++ tests/Refinery/KindlyTo/Transformation/DictionaryTransformationTest.php
@@ -32,6 +32,32 @@ class DictionaryTransformationTest extends TestCase
         $this->assertEquals($expectedVal, $transformedValue);
     }
 
+    /**
+     * @dataProvider TransformationFailingDataProvider
+     * @param $failingVal
+     */
+    public function testTransformationFailures($failingVal)
+    {
+        $this->expectNotToPerformAssertions();
+        $transformation = new DictionaryTransformation(new StringTransformation());
+        try {
+            $result = $transformation->transform($failingVal);
+        }catch(ConstraintViolationException $exception)
+        {
+            return;
+        }
+        $this->fail();
+    }
+
+    public function TransformationFailingDataProvider()
+    {
+        return [
+            'key_not_a_string' => ['hello'],
+            'value_not_a_string' => ['hello' => 1],
+            'empty_array' => [array()]
+        ];
+    }
+
     public function DictionaryTransformationDataProvider()
     {
         return [
