154ecfabb249062bf3d96de3a43101f6c0fb9bed
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-14 08:56:06 +0200

    Check instance of.

--- tests/Refinery/KindlyTo/Transformation/ListTransformationTest.php
+++ tests/Refinery/KindlyTo/Transformation/ListTransformationTest.php
@@ -26,6 +26,7 @@ class ListTransformationTest extends TestCase
     {
         $transformList = new ListTransformation(new StringTransformation());
         $transformedValue = $transformList->transform(array(self::first_arr, self::second_arr));
+        $this->assertInstanceOf(ListTransformation::class, $transformedValue);
         $this->assertEquals(array(self::first_arr, self::second_arr), $transformedValue);
     }
 
