d4a168e5358ac38ac43c220d6bfe7d6246d1e1d0
Richard Klees <richard.klees@concepts-and-training.de>
2024-01-25 16:13:57 +0100

    Setup: get agents via component mechanism

--- components/ILIAS/Repository/Repository.php
+++ components/ILIAS/Repository/Repository.php
@@ -32,6 +32,9 @@ class Repository implements Component\Component
         array | \ArrayAccess &$pull,
         array | \ArrayAccess &$internal,
     ): void {
-        // ...
+        $contribute[\ILIAS\Setup\Agent::class] = fn() =>
+            new \ilRepositorySetupAgent(
+                $pull[\ILIAS\Refinery\Factory::class]
+            );
     }
 }
