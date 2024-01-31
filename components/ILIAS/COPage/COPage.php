98cb0b1e28ab658ad390dd7d20e023e471c93f52
Richard Klees <richard.klees@concepts-and-training.de>
2024-01-31 19:23:57 +0100

    Component: build public resources (WIP)

--- components/ILIAS/COPage/COPage.php
+++ components/ILIAS/COPage/COPage.php
@@ -32,6 +32,9 @@ class COPage implements Component\Component
         array | \ArrayAccess &$pull,
         array | \ArrayAccess &$internal,
     ): void {
-        // ...
+        $contribute[Component\Resource\PublicAsset::class] = fn() =>
+            new Component\Resource\ComponentJS($this, "ilCOPagePres.js");
+        $contribute[Component\Resource\PublicAsset::class] = fn() =>
+            new Component\Resource\ComponentJS($this, "ilCOPagePCInteractiveImage.js");
     }
 }
--- components/ILIAS/Notifications/Notifications.php
+++ components/ILIAS/Notifications/Notifications.php
@@ -32,6 +32,11 @@ class Notifications implements Component\Component
         array | \ArrayAccess &$pull,
         array | \ArrayAccess &$internal,
     ): void {
-        // ...
+        $contribute[Component\Resource\PublicAsset::class] = fn() =>
+            new Component\Resource\ComponentJS($this, "notifications.js");
+        $contribute[Component\Resource\PublicAsset::class] = fn() =>
+            new Component\Resource\ComponentJS($this, "browser_notifications.js");
+        $contribute[Component\Resource\PublicAsset::class] = fn() =>
+            new Component\Resource\ComponentCSS($this, "osd.css");
     }
 }
