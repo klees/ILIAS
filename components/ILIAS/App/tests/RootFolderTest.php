6ab4c518f241fa9e383ebdb42128ceffcf7cbae4
daniwe4 <daniel.weise@concepts-and-training.de>
2024-09-03 14:02:33 +0200

    trunk Authentication:0041779 add a session max idle set objective

--- components/ILIAS/App/tests/RootFolderTest.php
+++ components/ILIAS/App/tests/RootFolderTest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +15,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 namespace ILIAS\App\tests;
 
@@ -51,6 +51,8 @@ final class RootFolderTest extends TestCase
         'package.json',
         'README.md',
         'unzip_test_file.zip',
+        'webdav.php',
+        'info.php',
         '.DS_Store',
         '.buildpath',
         '.project'
